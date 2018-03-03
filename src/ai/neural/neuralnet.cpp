b8d2182d4bb3770c62537633d4b7b7a619329533
Florent Uguet <florent.uguet@gmail.com>
2018-03-03 17:39:03 +0100

    Updated NN functions

--- src/ai/neural/neuralnet.cpp
+++ src/ai/neural/neuralnet.cpp
@@ -43,6 +43,13 @@ void NeuralNet::init(std::vector<int> topology)
         }
     }
 
+    //Set the bias nodes
+    for(unsigned int x=1;x<topology.size()-1;x++)
+        this->nodes[x][topology[x]] = BIAS_VALUE;
+
+    this->layer_count = topology.size();
+    this->output_layer = this->layer_count-1;
+
     //Output count
     std::cout << "Network initialized with " << this->node_count << " nodes and " << this->weight_count << " weights";
 }
@@ -51,11 +58,11 @@ void NeuralNet::setWeights(std::vector<float> weights)
 {
     int c = 0;
 
-    for(int x=0; x<this->weights.size();x++)
+    for(unsigned x=0; x<this->weights.size();x++)
     {
-        for(int y=0; y<this->weights[x].size();y++)
+        for(unsigned y=0; y<this->weights[x].size();y++)
         {
-            for(int z=0; z<this->weights[x][y].size();z++)
+            for(unsigned z=0; z<this->weights[x][y].size();z++)
             {
                 this->weights[x][y][z] = weights[c];
                 c++;
@@ -64,7 +71,83 @@ void NeuralNet::setWeights(std::vector<float> weights)
     }
 }
 
+std::vector<float> NeuralNet::getWeights()
+{
+    std::vector<float> w;
+
+    for(unsigned x=0; x<this->weights.size();x++)
+        for(unsigned y=0; y<this->weights[x].size();y++)
+            for(unsigned z=0; z<this->weights[x][y].size();z++)
+                w.push_back(this->weights[x][y][z]);
+
+    return w;
+}
+
 std::vector<float> NeuralNet::process(std::vector<float> inputs)
 {
-    //Process each layer
+    //Set the inputs
+    for(unsigned i=0; i<inputs.size(); i++)
+        this->nodes[0][i] = inputs[i];
+
+    //Process each layer except the input one
+    for(unsigned x=1; x<this->layer_count; x++)
+    {
+        //Current Layer Count : Number of nodes to calculate on the current layer
+        unsigned clc = topology[x];
+
+        //Current Layer Count : Number of nodes on the previous layer
+        unsigned plc = x-1 == 0 ? topology[x-1] : topology[x-1] + 1;
+
+        //For each node in the current layer
+        for(unsigned y = 0; y < clc; y++)
+        {
+            float sum = 0.f;
+
+            //For each node in the previous layer
+            for(unsigned z = 0; z < plc; z++)
+            {
+                sum += nodes[x][z] * weights[x][z][y];
+            }
+
+            this->nodes[x][y] = sigmoid(sum);
+        }
+    }
+
+    //Get the outputs
+    return this->getOutputs();
+}
+
+float NeuralNet::transferFunction(float x)
+{
+    return sigmoid(x);
+}
+
+float NeuralNet::sigmoid(float x)
+{
+    return tanh(x);
+}
+
+float NeuralNet::relu(float x)
+{
+    return x <= 0 ? 0 : x;
+}
+
+float NeuralNet::transferFunctionh(float x)
+{
+    return sigmoidh(x);
+}
+
+float NeuralNet::sigmoidh(float x)
+{
+    return 1.f - x*x;
+}
+
+float NeuralNet::reluh(float x)
+{
+    return x <= 0 ? 0 : 1;
+}
+
+std::vector<float> NeuralNet::getOutputs()
+{
+    return this->nodes.back();
 }
--- src/ai/neural/neuralnet.h
+++ src/ai/neural/neuralnet.h
@@ -3,12 +3,15 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 template <class T>
 using Array2D = std::vector<std::vector<T>>;
 
 template <class T>
 using Array3D = std::vector<Array2D<T>>;
+
+#define BIAS_VALUE 1.0f
 
 class NeuralNet
 {
@@ -17,15 +20,32 @@ public:
     void init(std::vector<int> topology);
 
     void setWeights(std::vector<float> weights);
+    std::vector<float> getWeights();
     std::vector<float> process(std::vector<float> inputs);
+
+    std::vector<float> getOutputs();
+
+    static float transferFunction(float x);
+    static float sigmoid(float x);
+    static float relu(float x);
+
+    static float transferFunctionh(float x);
+    static float sigmoidh(float x);
+    static float reluh(float x);
 
 private:
     std::vector<int> topology;
     Array2D<float> nodes;
     Array3D<float> weights;
 
+    Array2D<float> gradients;
+    Array3D<float> delta_weights;
+
     int node_count = 0;
     int weight_count = 0;
+
+    unsigned layer_count = 0;
+    unsigned output_layer = 0;
 };
 
 #endif // NEURALNET_H
